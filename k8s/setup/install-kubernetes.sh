2ea958749e0f998244fd244c680b2392a6fcefb6
root <root@k8s-master.us-central1-a.c.sapient-office-218912.internal>
2019-05-22 04:33:10 +0000

    K8s Updated

--- k8s/setup/install-kubernetes.sh
+++ k8s/setup/install-kubernetes.sh
@@ -21,10 +21,10 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet=1.10.4-00 kubeadm=1.10.4-00 kubectl=1.10.4-00
+apt-get install -y kubelet kubeadm kubectl
 
 echo "deploying kubernetes (with calico)..."
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.0.100" 
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="10.128.0.55" 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 # DigitalOcean without firewall (IP-in-IP allowed) - or any other cloud / on-prem that supports IP-in-IP traffic
